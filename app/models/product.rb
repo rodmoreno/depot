93fa9d6f6d6365805dd149e7c474fa06777f90b8
Rodrigo Moreno <moreno.rdr@gmail.com>
2023-08-19 19:54:48 -0600

    feature: add relation between Product and LineItem
    Also added a function to prevent delete a product into a LineItem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ApplicationRecord
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, length: { minimum: 10, message: 'must be at least 10 characters' }
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
@@ -7,4 +10,13 @@ class Product < ApplicationRecord
     with: /\.(gif|jpg|png)\z/i,
     message: 'must be a URL for GIF, JPG or PNG image'
   }
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw :abort
+    end
+  end
 end
