d402b78dd17f7e8869b1f96e1be43a8ce98cf00f
Rodrigo Moreno <moreno.rdr@gmail.com>
2023-09-06 00:17:54 -0600

    feature: create charge! method to execute pago lib

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
+require 'pago'
+
 class Order < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
@@ -7,6 +9,9 @@ class Order < ApplicationRecord
     "Purchase order" => 2
   }
 
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: pay_types.keys
+
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
       item.cart_id = nil
@@ -14,6 +19,36 @@ class Order < ApplicationRecord
     end
   end
 
-  validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: pay_types.keys
+  def charge!(pay_type_params)
+    payment_details = {}
+    payment_method = nil
+
+    case pay_type
+    when 'Check'
+      payment_method = :check
+      payment_details[:routing] = pay_type_params[:routing_number]
+      payment_details[:account] = pay_type_params[:account_number]
+
+    when 'Credit card'
+      payment_method = :credit_card
+      month, year = pay_type_params[:expiration_date].split(//)
+      payment_details[:cc_num] = pay_type_params[:credit_card_number]
+      payment_details[:expiration_month] = month
+      payment_details[:expiration_year] = year
+
+    when 'Purchase order'
+      payment_method = :po
+      payment_details[:po_num] = pay_type_params[:po_number]
+    end
+
+    payment_result = Pago.make_payment(
+      order_id: id,
+      payment_method:,
+      payment_details:
+    )
+
+    raise payment_result.error unless payment_result.succeeded?
+
+    OrderMailer.received(self).deliver_later
+  end
 end
